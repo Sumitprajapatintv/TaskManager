275c0d1b73c71ee2209a030ea5afd93eb5132da8
abc <abc@example.com>
2023-05-26 19:07:08 +0530

    Add another API

--- controller/taskcontroller.js
+++ controller/taskcontroller.js
@@ -1,6 +1,23 @@
 const getAllTask = (req, res) => {
-  res.send("All task Here Hai");
+  res.send("Get all Task");
 };
+const createTask = (req, res) => {
+  res.send("create All task");
+};
+const getTask = (req, res) => {
+  res.send("create Single Task");
+};
+const updateTask = (req, res) => {
+  res.send("update Task");
+};
+const deleteTask = (req, res) => {
+  res.send("Delete Task");
+};
+
 module.exports = {
   getAllTask,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
--- routes/task.js
+++ routes/task.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express();
-const { getAllTask } = require("../controller/taskcontroller");
-router.route("/").get(getAllTask);
+const {
+  getAllTask,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/taskcontroller");
+router.route("/").get(getAllTask).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 module.exports = router;
